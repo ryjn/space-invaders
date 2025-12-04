3ebcad20d99424dcc900ecf77740ec0d8034bbfd
Ryan Kim <ryanjnkim@gmail.com>
2025-12-04 00:13:04 -0500

    buffer

--- main.cpp
+++ main.cpp
@@ -7,10 +7,43 @@ void error_callback(int error, const char* description) {
   fprintf(stderr, "Error: %s\n", description);
 }
 
-int main() {
-  glfwSetErrorCallback(error_callback);
+struct Buffer {
+  size_t width, height;
+  uint32_t* data;
+};
 
-  GLFWwindow* window;
+/**
+ * @brief Converts RGB color values to a 32-bit unsigned integer 
+ *
+ * Sets left-most 24 bits to r, g, and b values, respectively.
+ * 8 right-most bits are set to 255
+ * Format: RGBA
+ *
+ * @param r Red component (0 - 255)
+ * @param g Green component (0 - 255)
+ * @param b Blue component (0 - 255)
+ * @return uint32_t Combined RGBA value
+ */
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b) {
+  return  (r << 24) |
+          (g << 16) |
+          (b << 8)  |
+          255;
+}
+
+void buffer_clear(Buffer* buffer, uint32_t color) {
+  for(size_t i = 0; i < buffer->width * buffer->height; ++i) {
+    buffer->data[i] = color;
+  }
+}
+
+
+int main() {
+  const size_t buffer_width = 224;
+  const size_t buffer_height = 256;
+
+  // Set callback for errors
+  glfwSetErrorCallback(error_callback);
 
   // Initialize the library
   if (!glfwInit()) return -1;
@@ -22,7 +55,7 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
   // Create window
-  window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
   if(!window) {
     glfwTerminate();
     return -1;
@@ -44,8 +77,17 @@ int main() {
 
   printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
 
-  // Game loop
   glClearColor(1.0, 0.0, 0.0, 1.0);
+
+  // Initialize buffer
+  Buffer buffer;
+  buffer.width = buffer_width;
+  buffer.height = buffer_height;
+  buffer.data = new uint32_t[buffer.width * buffer.height];
+
+  buffer_clear(&buffer, 0);
+
+  // Game loop
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
